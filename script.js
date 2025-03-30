577bc07b081e6a53276d93e0fadd2da19e5f076d
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-31 09:04:14 +1100

    Create operate function The operate function will take three variables: operator, number and another number. It will call one of the functions on the numbers based on the operator.

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let number;
+let operator;
+let anotherNumber;
+
+function operate(operator, number, anotherNumber) {
+    if (operator === "+") {
+        add(number, anotherNumber);
+    } else if (operator === "-") {
+        subtract(number, anotherNumber);
+    } else if (operator === "*") {
+        multiply(number, anotherNumber);
+    } else if (operator === "/") {
+        divide(number, anotherNumber);
+    }
+}
